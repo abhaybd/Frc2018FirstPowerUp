59f30cf8a0bedb7fb66c80da76c9da411d5aab47
Abhay D <a.deshpande012@gmail.com>
2018-08-13 16:17:11 -0700

    Fixed the limitation where all parameters had to be the same type, added unboxing
    The limitation was caused by the Unity object serialization, which couldn't handle objects. I started using JSON.net, which made it good.

--- src/test/RemoteSwerve.java
+++ src/test/RemoteSwerve.java
@@ -9,12 +9,24 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RemoteSwerve
 {
     public static void main(String[] args) throws IOException
     {
+        Map<Class<?>,Class<?>> unboxMap = new HashMap<>();
+        unboxMap.put(Double.class, double.class);
+        unboxMap.put(Integer.class, int.class);
+        unboxMap.put(Float.class, float.class);
+        unboxMap.put(Long.class, long.class);
+        unboxMap.put(Boolean.class, boolean.class);
+        unboxMap.put(Character.class, char.class);
+        unboxMap.put(Byte.class, byte.class);
+        unboxMap.put(Short.class, short.class);
+
         double width = 39.0;
         double length = 39.0;
 
@@ -42,50 +54,58 @@ public class RemoteSwerve
             lfModule, rfModule, lrModule, rrModule);
 
         TrcTaskMgr taskMgr = TrcTaskMgr.getInstance();
-
-        System.out.println("Waiting for connection...");
         ServerSocket serverSocket = new ServerSocket(4444);
-        Socket socket = serverSocket.accept();
-        System.out.println("Received connection from " + socket.getInetAddress().toString());
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintStream out = new PrintStream(socket.getOutputStream());
-
-        Gson gson = new Gson();
 
         while(true)
         {
-            String line = in.readLine();
-            RPCRequest request = gson.fromJson(line, new TypeToken<RPCRequest>(){}.getType());
-            System.out.println("Received request: " + line);
-            try
+            System.out.println("Waiting for connection...");
+            Socket socket = serverSocket.accept();
+            System.out.println("Received connection from " + socket.getInetAddress().toString());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream out = new PrintStream(socket.getOutputStream());
+
+            Gson gson = new Gson();
+
+            while(true)
             {
+                String line = in.readLine();
+                if(line == null)
+                {
+                    socket.close();
+                    break;
+                }
+                RPCRequest request = gson.fromJson(line, new TypeToken<RPCRequest>(){}.getType());
+                System.out.println("Received request: " + line);
+                try
+                {
+                    Class<?>[] classes = request.args.stream().map(e -> unboxMap.get(e.getClass())).toArray(Class<?>[]::new);
+                    Method method = driveBase.getClass().getMethod(request.methodName, classes);
+                    System.out.printf("Invoking method %s.%s(%s)\n", "driveBase", request.methodName, request.args.toString());
+                    method.invoke(driveBase, request.args.toArray());
 
-                Method method = driveBase.getClass().getMethod(request.methodName, double.class, double.class, double.class);
-                System.out.printf("Invoking method %s.%s(%s)\n", "driveBase", request.methodName, request.args.toString());
-                method.invoke(driveBase, request.args.toArray());
+                    SwerveStatus status = new SwerveStatus();
+                    status.lfPower = (float) lfModule.getDrivePower();
+                    status.rfPower = (float) rfModule.getDrivePower();
+                    status.lrPower = (float) lrModule.getDrivePower();
+                    status.rrPower = (float) rrModule.getDrivePower();
 
-                SwerveStatus status = new SwerveStatus();
-                status.lfPower = (float) lfModule.getDrivePower();
-                status.rfPower = (float) rfModule.getDrivePower();
-                status.lrPower = (float) lrModule.getDrivePower();
-                status.rrPower = (float) rrModule.getDrivePower();
+                    status.lfAngle = (float) lfModule.getTargetAngle();
+                    status.rfAngle = (float) rfModule.getTargetAngle();
+                    status.lrAngle = (float) lrModule.getTargetAngle();
+                    status.rrAngle = (float) rrModule.getTargetAngle();
 
-                status.lfAngle = (float) lfModule.getAngle();
-                status.rfAngle = (float) rfModule.getAngle();
-                status.lrAngle = (float) lrModule.getAngle();
-                status.rrAngle = (float) rrModule.getAngle();
+                    String response = gson.toJson(status);
+                    System.out.println("Sending response: " + response);
+                    out.println(response);
+                    out.flush();
+                }
+                catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
+                {
+                    e.printStackTrace();
+                }
 
-                String response = gson.toJson(status);
-                System.out.println("Sending response: " + response);
-                out.println(response);
-                out.flush();
+                taskMgr.executeTaskType(TrcTaskMgr.TaskType.POSTCONTINUOUS_TASK, TrcRobot.RunMode.TELEOP_MODE);
             }
-            catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
-            {
-                e.printStackTrace();
-            }
-
-            taskMgr.executeTaskType(TrcTaskMgr.TaskType.POSTCONTINUOUS_TASK, TrcRobot.RunMode.TELEOP_MODE);
         }
     }
 
